cf75e28eafbc0737014741a91e68b4b1c7716fb4
ilaydakurtboun <115426763+ilaydakurtboun@users.noreply.github.com>
2022-12-23 14:47:16 +0300

    Update admin.py

--- app/user/admin.py
+++ app/user/admin.py
@@ -3,5 +3,5 @@ from .models import User, ResetPassword,Friends
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(User)
+admin.site.register(ResetPassword)
 admin.site.register(Friends)
