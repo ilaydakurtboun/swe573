9ea49bfcf284bbf31ca2bf94f01234f6e42639d6
ilaydakurtboun <115426763+ilaydakurtboun@users.noreply.github.com>
2022-12-23 14:46:06 +0300

    Update admin.py

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, ResetPassword
+from .models import User, ResetPassword,Friends
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(ResetPassword)
+admin.site.register(User)
+admin.site.register(Friends)
